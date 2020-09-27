a8389a424cfa7ec21770a5b06b3d7032b971f8d8
KarenDaviesE <davies.karen10@gmail.com>
2020-09-27 20:35:25 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+      inv <- NULL
+      set <- function(y){
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() {x}
+      setInverse <- function(inverse) {inv <<- inverse}
+      getInverse <- function() {inv}
+      list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+      inv <- x$getInverse()
+      if(!is.null(inv)){
+            message("getting cached data")
+            return(inv)
+      }
+      mat <- x$get()
+      inv <- solve(mat, ...)
+      x$setInverse(inv)
+      inv
 }
+
+source("makeCacheMatrix.R")
+setwd("C:/Users/karda/Desktop/GEO/Geoestadística/John Hopkins University - R/ProgrammingAssignment2")
+pmatrix<-makeCacheMatrix(matrix)
+pmatrix<-makeCacheMatrix(matrix(1:16,nrow=4, ncol=4))
+pmatrix$get()
+pmatrix$getInverse()
+pmatrix<-makeCacheMatrix(matrix(1:4,nrow = 2,ncol = 2))
+pmatrix$get()
+pmatrix$getInverse()
+cacheSolve(pmatrix)
+cacheSolve(pmatrix)
+pmatrix$getInverse()
+
